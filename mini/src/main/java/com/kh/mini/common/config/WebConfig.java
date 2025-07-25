c5ab6c25f51d8226777318fdc59811a0194d64cf
dudwo2199 <dudwo2199@gmail.com>
2025-07-25 11:47:02 +0900

    [CHG] 경로

--- mini/src/main/java/com/kh/mini/common/config/WebConfig.java
+++ mini/src/main/java/com/kh/mini/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://192.168.20.171:5501", "http://127.0.0.1:5501")
+                .allowedOrigins("http://127.0.0.1:5500", "http://192.168.20.171:5501", "http://127.0.0.1:5501", "https://dudwo2199.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
